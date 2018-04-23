79e77ece02b3fc40f49e591103b553f6d48d9e2f
GideonvDoorn <gideon.vandoorn@student.fontys.nl>
2018-04-23 15:02:44 +0200

    voor rik

--- bank/src/test/java/Assignments/Assignment_01.java
+++ bank/src/test/java/Assignments/Assignment_01.java
@@ -1,23 +1,36 @@
 package Assignments;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import util.DatabaseCleaner;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Assignment_01 {
+
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bankPU");
+
+
     @BeforeEach
     void setUp() {
 
-        DatabaseCleaner dbc = new DatabaseCleaner(/* here is entit ymanag er */);
+        EntityManager em = emf.createEntityManager();
+        DatabaseCleaner dbc = new DatabaseCleaner(em);
         try{
             dbc.clean();
         }
         catch(Exception e){
             e.printStackTrace();
         }
+
+    }
+
+    @Test
+    void testtest(){
 
     }
 
